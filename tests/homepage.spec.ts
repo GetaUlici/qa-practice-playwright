c8041bde0e580720f3b59a8ed4240138ec61fbf5
geta.ulici <ulicigeta@gmail.com>
2025-10-23 17:58:43 +0300

    reformat homepage tests

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 import { HompePage } from '../pages/home.page';
-import { YoutubePage } from '../pages/youtube.page';
 const baseUrl = 'https://qa-practice.razvanvancea.ro/';
 
 test.describe('Homepage test suite', () => {
@@ -10,74 +9,56 @@ test.describe('Homepage test suite', () => {
 
   test('k6 "Click here" button test', async ({ page }) => {
     const homepage = new HompePage(page);
-    const popupPromise = page.waitForEvent('popup');
+
+    const page1Promise = page.waitForEvent('popup');
     await homepage.clickFirstClickHereBtn();
-    const youtubePage = await popupPromise;
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
 
-    const ytPage = new YoutubePage(youtubePage);
-
-    await ytPage.rejectBtn.click();
-    await ytPage.verifyk6LinkVisible();
+    await expect(page1.getByRole('link', { name: 'k6 - Load Testing' })).toBeVisible();
   });
-
-  // test.only('demo', async ({ page }) => {
-  //   const homepage = new HompePage(page);
-  //   const page1Promise = page.waitForEvent('popup');
-  //   await homepage.clickFirstClickHereBtn();
-  //   const page1 = await page1Promise;
-  //       const ytPage = new YoutubePage(youtubePage);
-
-
-  //   await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
-  //   await expect(page1.getByRole('link', { name: 'k6 - Load Testing' })).toBeVisible();
-
-  // });
 
   test('Playwright "Click here" button test', async ({ page }) => {
     const homepage = new HompePage(page);
-    const popupPromise = page.waitForEvent('popup');
+
+    const page1Promise = page.waitForEvent('popup');
     await homepage.clickSecondClickHereBtn();
-    const youtubePage = await popupPromise;
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
 
-    const ytPage = new YoutubePage(youtubePage);
-
-    await ytPage.rejectBtn.click();
-    await ytPage.verifyPwLinkVisible();
+    await expect(page1.getByRole('link', { name: 'Playwright - E2E Reliable' })).toBeVisible();
   });
 
   test('Cypress "Click here" button test', async ({ page }) => {
     const homepage = new HompePage(page);
-    const popupPromise = page.waitForEvent('popup');
+
+    const page1Promise = page.waitForEvent('popup');
     await homepage.clickThirdClickHereBtn();
-    const youtubePage = await popupPromise;
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
 
-    const ytPage = new YoutubePage(youtubePage);
-
-    await ytPage.rejectBtn.click();
-    await ytPage.verifyCyLinkVisible();
+    await expect(page1.getByRole('link', { name: 'E2E Testing - Cypress' })).toBeVisible();
   });
 
   test('TestCafe "Click here" button test', async ({ page }) => {
     const homepage = new HompePage(page);
-    const popupPromise = page.waitForEvent('popup');
+
+    const page1Promise = page.waitForEvent('popup');
     await homepage.clickFourthClickHereBtn();
-    const youtubePage = await popupPromise;
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
 
-    const ytPage = new YoutubePage(youtubePage);
-
-    await ytPage.rejectBtn.click();
-    await ytPage.verifyTestCafeLinkVisible();
+    await expect(page1.getByRole('link', { name: 'TestCafe - From 0 to Testing' })).toBeVisible();
   });
 
   test('PactumJs "Click here" button test', async ({ page }) => {
     const homepage = new HompePage(page);
-    const popupPromise = page.waitForEvent('popup');
+
+    const page1Promise = page.waitForEvent('popup');
     await homepage.clickFifthClickHereBtn();
-    const youtubePage = await popupPromise;
+    const page1 = await page1Promise;
+    await page1.getByRole('button', { name: 'Reject the use of cookies and' }).click();
 
-    const ytPage = new YoutubePage(youtubePage);
-
-    await ytPage.rejectBtn.click();
-    await ytPage.verifyPactumLinkVisible();
+    await expect(page1.getByRole('link', { name: 'API Automation - PactumJS' })).toBeVisible();
   });
 });
